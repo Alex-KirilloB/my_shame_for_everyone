26dfb9e27576d58cd0524bf27c718ba2173fde9d
Alex_Kirillov <kirillov_shurik@mail.ru>
2024-04-18 16:41:52 +0300

    ''

---
+++ bbpp/c07_caesar_hacker.py
@@ -0,0 +1,33 @@
+"""
+Взлом шифра Цезаря
+Взлом сообщений, зашифрованных шифром Цезаря
+путем перебора всех возможных ключей
+"""
+
+print('Caesar cipher hacker')
+
+# пользователь вводит сообщение
+print('Enter the encrypted message to hack')
+message = input('> ')
+
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(SYMBOLS)):
+    translated = ''
+
+    # Расшифровка
+    for symbol in message:
+        if symbol in SYMBOLS:
+            num = SYMBOLS.find(symbol)
+            num -= key
+
+            if num < 0:
+                num += len(SYMBOLS)
+
+            translated += SYMBOLS[num]
+
+        else:
+            translated += symbol
+
+    # Вывод ключа вместе с расшифрованным текстом
+    print(f'Key #{key}: {translated}')
